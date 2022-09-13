456a10e7fb4823d655c41001b1ce3fe6cf022d72
zhuicode99 <enlin291814833@hotmail.com>
2022-09-13 20:37:48 +0000

    add tweet count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,32 @@
-$(document).ready(function() {
-  // --- our code goes here ---
+$(document).ready(function() { /* to ensure DOM has loaded */
+  $("#tweet-text").on('input', function() { 
+    /* console.log(this); */
+    const charNum = $(this).val().length; ////track the number of input,this ===textarea,val()method track the length.
+
+    const counterElem = $(this).parent().children('.button-number').children('.counter');
+
+    const trackChar = 140 - charNum;
+
+    counterElem.text(trackChar);
+
+    if (trackChar < 0) {
+      counterElem.css('color', 'red');
+    } else {
+      counterElem.css('color', '#545149');
+    }
+
+  });
 });
+
+
+/* $('.counter')  *//* to target the counter ,use this ==text area*/
+
+/* make counter turn red when invalid */
+
+/* 
+const textArea = Document.getElementbyId(textarea)
+textArea.eventListener( 
+
+) */
+
+
